94497712ef6459a4011be551ed28e654c901db0c
Valera Shabanov <github@shasoft.com>
2024-11-05 21:59:12 +0300

    update class Dish

--- src/main/java/ru/shasoft/votingsystem/menu/Dish.java
+++ src/main/java/ru/shasoft/votingsystem/menu/Dish.java
@@ -4,6 +4,11 @@ public class Dish {
     public String name;
     public int price;
 
+    public Dish() {
+        name = "";
+        price = 0;
+    }
+
     public Dish(String name, int price) {
         this.name = name;
         this.price = price;
