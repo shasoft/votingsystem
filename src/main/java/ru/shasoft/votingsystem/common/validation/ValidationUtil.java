77bd9afead95f3ab0f83efc1a7f8cb131b33ad16
Valera Shabanov <github@shasoft.com>
2024-11-08 22:19:56 +0300

    add ValidationUtil.checkUpdateLike

--- src/main/java/ru/shasoft/votingsystem/common/validation/ValidationUtil.java
+++ src/main/java/ru/shasoft/votingsystem/common/validation/ValidationUtil.java
@@ -4,8 +4,11 @@ import lombok.experimental.UtilityClass;
 import ru.shasoft.votingsystem.common.HasId;
 import ru.shasoft.votingsystem.common.error.IllegalRequestDataException;
 
+import java.time.LocalTime;
+
 @UtilityClass
 public class ValidationUtil {
+    static final int VOTE_END_HOUR = 11;
 
     public static void checkNew(HasId bean) {
         if (!bean.isNew()) {
@@ -19,6 +22,12 @@ public class ValidationUtil {
             bean.setId(id);
         } else if (bean.id() != id) {
             throw new IllegalRequestDataException(bean.getClass().getSimpleName() + " must has id=" + id);
+        }
+    }
+
+    public static void checkUpdateLike() {
+        if (LocalTime.now().isAfter(LocalTime.of(VOTE_END_HOUR, 0))) {
+            throw new IllegalRequestDataException("It's too late to vote.");
         }
     }
 }
--- src/main/java/ru/shasoft/votingsystem/vote/web/VoteController.java
+++ src/main/java/ru/shasoft/votingsystem/vote/web/VoteController.java
@@ -5,10 +5,9 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import ru.shasoft.votingsystem.app.AuthUser;
-import ru.shasoft.votingsystem.common.error.IllegalRequestDataException;
+import ru.shasoft.votingsystem.common.validation.ValidationUtil;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 
 @RestController
 @RequestMapping(value = VoteController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -16,7 +15,6 @@ import java.time.LocalTime;
 public class VoteController extends AbstractVoteController {
 
     static final String REST_URL = "/api/vote";
-    static final int VOTE_END_HOUR = 11;
 
     @GetMapping("/{restaurant_id}")
     public boolean get(@PathVariable int restaurant_id, @AuthenticationPrincipal AuthUser authUser) {
@@ -26,18 +24,14 @@ public class VoteController extends AbstractVoteController {
     @PutMapping(value = "/{restaurant_id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void like(@PathVariable int restaurant_id, @AuthenticationPrincipal AuthUser authUser) {
-        if (LocalTime.now().isAfter(LocalTime.of(VOTE_END_HOUR, 0))) {
-            throw new IllegalRequestDataException("It's too late to vote.");
-        }
+        ValidationUtil.checkUpdateLike();
         repository.like(restaurant_id, LocalDate.now(), authUser.id());
     }
 
     @DeleteMapping(value = "/{restaurant_id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void unlike(@PathVariable int restaurant_id, @AuthenticationPrincipal AuthUser authUser) {
-        if (LocalTime.now().isAfter(LocalTime.of(VOTE_END_HOUR, 0))) {
-            throw new IllegalRequestDataException("It's too late to vote.");
-        }
+        ValidationUtil.checkUpdateLike();
         repository.unlike(restaurant_id, LocalDate.now(), authUser.id());
     }
 }
