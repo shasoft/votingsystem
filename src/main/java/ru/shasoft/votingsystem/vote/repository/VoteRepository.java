aea489e55516e1b6bdd342ecc1b52f8569fb9a66
Valera Shabanov <github@shasoft.com>
2024-11-11 15:29:39 +0300

    add Menu.votes to VoteRepository

--- src/main/java/ru/shasoft/votingsystem/vote/repository/VoteRepository.java
+++ src/main/java/ru/shasoft/votingsystem/vote/repository/VoteRepository.java
@@ -15,17 +15,32 @@ public interface VoteRepository extends BaseRepository<Vote> {
     @Query("SELECT v FROM Vote v WHERE v.restaurantId = :restaurantId and v.createAt = :create_at and v.userId = :user_id")
     Optional<Vote> findVote(int restaurantId, LocalDate create_at, int user_id);
 
-    @Transactional
-    default Vote like(int restaurantId, LocalDate create_at, int user_id) {
-        Optional<Vote> optVote = findVote(restaurantId, create_at, user_id);
-        if (optVote.isEmpty()) {
-            return save(new Vote(null, restaurantId, create_at, user_id));
-        }
-        return null;
-    }
-
     @Modifying
     @Transactional
     @Query("DELETE FROM Vote v WHERE v.restaurantId = :restaurantId and v.createAt = :create_at and v.userId = :user_id")
-    void unlike(int restaurantId, LocalDate create_at, int user_id);
+    int deleteVote(int restaurantId, LocalDate create_at, int user_id);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Menu m SET m.votes = m.votes + :votes WHERE m.restaurant.id = :restaurantId and m.createAt = :create_at")
+    void updateVotes(int restaurantId, LocalDate create_at, int votes);
+
+    @Transactional
+    default Vote like(int restaurantId, LocalDate create_at, int user_id) {
+        Vote ret = null;
+        Optional<Vote> optVote = findVote(restaurantId, create_at, user_id);
+        if (optVote.isEmpty()) {
+            ret = save(new Vote(null, restaurantId, create_at, user_id));
+            updateVotes(restaurantId, create_at, 1);
+        }
+        return ret;
+    }
+
+    @Transactional
+    default void unlike(int restaurantId, LocalDate create_at, int user_id) {
+        final int updates = deleteVote(restaurantId, create_at, user_id);
+        if (updates != 0) {
+            updateVotes(restaurantId, create_at, -1);
+        }
+    }
 }
